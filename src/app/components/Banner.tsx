bb13ee96eccaf23a0d465c1dab942265e10bf5a3
Chris-Duignan <cduignan.1993@gmail.com>
2023-10-13 13:18:29 +0100

    feat: add navbar pathname logic

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -2,34 +2,39 @@
 
 import PrimaryTitle from "../../typography/PrimaryTitle";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Banner() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
+
+  const paths: {[key:string]: string} = {
+    "/" : "Home",
+    "/about-us": "Meet the Team",
+    "/about-bb": "About" 
+  }
 
   const handleOpen =() => {
     setIsOpen(!isOpen)
+    console.log
   }
+
+  const pathname = usePathname()
 
   return (
     <header className="grid grid-cols-3 items-center w-full h-[10vh] border-b-2 border-white">
       <PrimaryTitle style={"col-span-2"}>Bootcamp Benchmark</PrimaryTitle>
-      <nav className="bg-[#6B4D57] h-full">
-        <div>
+      <nav className="flex flex-col items-center justify-center bg-[#6B4D57] h-full">
           <div onClick={handleOpen}>
-            <Link href="/">Home</Link>
+            <p>{paths[pathname]}</p>
           </div>
           {isOpen ? (
             <>
-              <div>
-                <Link href="/about-bb">About BB</Link>
-              </div>
-              <div>
-                <Link href="/about-us">About Us</Link>
-              </div>
+              {Object.keys(paths).map((key) => {
+                return <Link href={key}>{paths[key]}</Link>
+              })}
             </>
           ) : null}
-        </div>
       </nav>
     </header>
   );
