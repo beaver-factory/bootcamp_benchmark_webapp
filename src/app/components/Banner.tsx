8f2a60368d773149412c6f67b4589fc63223b9d6
Chris-Duignan <cduignan.1993@gmail.com>
2023-10-13 12:34:18 +0100

    feat: add initial navbar with open feature

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import PrimaryTitle from "../../typography/PrimaryTitle";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 export default function Banner() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen =() => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <header className="w-100 h-10">
-      <h1>Bootcamp Benchmark</h1>
+    <header className="grid grid-cols-3 items-center w-full h-[10vh] border-b-2 border-white">
+      <PrimaryTitle style={"col-span-2"}>Bootcamp Benchmark</PrimaryTitle>
+      <nav className="bg-[#6B4D57] h-full">
+        <div>
+          <div onClick={handleOpen}>
+            <Link href="/">Home</Link>
+          </div>
+          {isOpen ? (
+            <>
+              <div>
+                <Link href="/about-bb">About BB</Link>
+              </div>
+              <div>
+                <Link href="/about-us">About Us</Link>
+              </div>
+            </>
+          ) : null}
+        </div>
+      </nav>
     </header>
   );
 }
