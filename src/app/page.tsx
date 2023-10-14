3911d806d1c7ee75eb4888fac7287236fcc23ab1
Alex Cupit <alexcupit@outlook.com>
2023-10-14 11:52:09 +0100

    feat: render skills and powerbi on homapage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import { getSkills } from '@/models/skills.models';
 
-export default function Home() {
+export default async function Home() {
+  const skills = await getSkills();
+  console.log(skills);
+
   return (
     <main>
-      <h1>HOME</h1>;
+      <h1>HOME</h1>
+      <h2>skills</h2>
+      {skills
+        ? skills.map((skill) => {
+            return (
+              <ol>
+                <li>{skill}</li>
+              </ol>
+            );
+          })
+        : 'no skills found'}
+      <iframe
+        title="Report Section"
+        width="600"
+        height="373.5"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNzMyNzUxYTktYzYzNy00ODgwLWFhMDctMTJkOTYxZDE1MDViIiwidCI6ImU3YjFjYTU3LTU1M2YtNDJmMC1hNDhhLTE1ODQxNWI2MDk1YiJ9"
+        allowFullScreen={true}
+      ></iframe>
     </main>
   );
 }
