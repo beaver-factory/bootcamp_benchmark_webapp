b4127978aacd47614d887d0b88bd5e347ad13dc8
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-10-31 16:48:28 +0000

    fix: layout

--- src/app/about-us/components/aboutUsBio.tsx
+++ src/app/about-us/components/aboutUsBio.tsx
@@ -1,7 +1,7 @@
 export default function aboutUsBio() {
   return (
-    <section className="w-[90vw] flex justify-center items-center bg-white">
-      <p className="border-solid border-black border-2 p-10 mb-10 w-[60%]">
+    <section className="w-[100vw] h-max flex justify-center align-center ">
+      <p className="border-solid border-black border-2 p-10 mb-5 w-[60%] h-max bg-white">
         We're an eager team of enthusiastic programmers, working across multiple projects both professionally and for personal development. See our bios below for more information, with social media and portfolio links - get in touch!
       </p>
     </section>
