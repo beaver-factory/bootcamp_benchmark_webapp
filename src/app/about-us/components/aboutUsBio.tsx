f996be515971563937a130d84fb2b2247497ff0f
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-10-31 16:22:12 +0000

    feat: sensible aboutUs

--- src/app/about-us/components/aboutUsBio.tsx
+++ src/app/about-us/components/aboutUsBio.tsx
@@ -2,8 +2,7 @@ export default function aboutUsBio() {
   return (
     <section className="w-[90vw] flex justify-center items-center bg-white">
       <p className="border-solid border-black border-2 p-10 mb-10">
-        We are an eager team of fantastic overachievers who absolutely destroy
-        any problem ever, we're the best.
+        We're an eager team of enthusiastic programmers, working across multiple projects both professionally and for personal development. See our bios below for more information, with social media and portfolio links - get in touch!
       </p>
     </section>
   );
